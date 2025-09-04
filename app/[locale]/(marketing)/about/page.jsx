7fefd25a73ccc8d583a5120eff66c449edcd1eab
aurelioo29 <aureliolucio5@gmail.com>
2025-09-04 12:44:05 +0700

    feat(brand): add about logo personal brand with responsive

---
+++ app/[locale]/(marketing)/about/page.jsx
@@ -0,0 +1,11 @@
+import AboutBrand from "@/app/components/ui/AboutBrand";
+import React from "react";
+
+export default function AboutPage() {
+  return (
+    <section>
+      {/* About Brand */}
+      <AboutBrand />
+    </section>
+  );
+}
