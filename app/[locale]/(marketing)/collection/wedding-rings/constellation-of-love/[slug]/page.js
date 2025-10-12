865d9e04a7ad23902c04ff6c0b267d9994aa0793
aurelioo29 <aureliolucio5@gmail.com>
2025-10-12 20:18:35 +0700

    feat: add col pages & detail pages

---
+++ app/[locale]/(marketing)/collection/wedding-rings/constellation-of-love/[slug]/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { notFound } from "next/navigation";
+import { COL_ITEMS } from "@/app/data/wedding";
+import DetailCatalog from "@/app/components/ui/catalog/wedding/DetailCatalog";
+
+export default async function DetailCOLPageCatalog({ params }) {
+  const { slug } = await params;
+
+  const item = COL_ITEMS.find((x) => x.slug === slug);
+  if (!item) return notFound();
+
+  return <DetailCatalog item={item} scope="col" />;
+}
